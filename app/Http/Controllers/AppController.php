048361311f54fc26f1182bfd5e63db25db6f6f19
Jonathan Torres <jonathantorres41@gmail.com>
2017-05-11 20:22:13 -0400

    Add comment.

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -6,6 +6,11 @@ use Illuminate\Http\Request;
 
 class AppController extends Controller
 {
+    /**
+     * Show the main index view.
+     *
+     * @return Illuminate\View\View
+     */
     public function index()
     {
         return view('index');
