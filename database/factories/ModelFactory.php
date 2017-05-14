dd8188d478b154ee30affd510631aeb086906791
Jonathan Torres <jonathantorres41@gmail.com>
2017-05-14 15:15:49 -0400

    Fix user model factory.

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -16,7 +16,7 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
     static $password;
 
     return [
-        'name' => $faker->name,
+        'name' => $faker->firstName,
         'last_name' => $faker->lastName,
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
