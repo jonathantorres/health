fafda736e987a59519df95c7ef6ee029e47fc82e
Jonathan Torres <jonathantorres41@gmail.com>
2017-06-15 21:49:31 -0400

    Implement soft deletes on the users table.

--- app/User.php
+++ app/User.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class User extends Authenticatable
 {
     use Notifiable;
+    use SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
@@ -26,4 +27,11 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = ['deleted_at'];
 }
