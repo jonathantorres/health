dde3c809d3286159073ff3a920b03cf7e1e952bb
Jonathan Torres <jonathantorres41@gmail.com>
2017-06-15 20:54:35 -0400

    Change pagination to 20 results per page.

--- app/Http/Controllers/BloodPressureController.php
+++ app/Http/Controllers/BloodPressureController.php
@@ -53,7 +53,7 @@ class BloodPressureController extends Controller
      */
     public function all()
     {
-        $readings = BloodPressure::orderBy('reading_date', 'desc')->paginate(10);
+        $readings = BloodPressure::orderBy('reading_date', 'desc')->paginate(20);
 
         return view('blood_pressure.all', [
             'readings' => $readings,
