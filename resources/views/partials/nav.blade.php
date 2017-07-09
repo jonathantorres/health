df55e29e7ed4ccbe99e589ed4aebaebf409c12c5
Jonathan Torres <jonathantorres41@gmail.com>
2017-07-09 00:38:46 -0400

    Remove weight from main menu.

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -31,22 +31,6 @@
                         </li>
                     </ul>
                 </li>
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-                        Weight <span class="caret"></span>
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li>
-                            <a href="#">Enter new weight</a>
-                        </li>
-                        <li>
-                            <a href="#">One</a>
-                        </li>
-                        <li>
-                            <a href="#">Two</a>
-                        </li>
-                    </ul>
-                </li>
             </ul>
 
             <!-- Right Side Of Navbar -->
