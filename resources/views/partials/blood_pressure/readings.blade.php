59a5ffd5f6ee0851267ed67536b51fa7cefb2c82
Jonathan Torres <jonathantorres41@gmail.com>
2017-08-10 20:40:32 -0400

    Add warning message if there are no blood pressure readings to display.

--- resources/views/partials/blood_pressure/readings.blade.php
+++ resources/views/partials/blood_pressure/readings.blade.php
@@ -1,35 +1,41 @@
 <div class="panel panel-default">
     <div class="panel-heading">{{ $title }}</div>
     <div class="panel-body">
-        <div class="table-responsive">
-            <table class="table table-striped table-hover table-condensed">
-                <thead>
-                    <tr>
-                        <th class="text-center">SYS</th>
-                        <th class="text-center">DIA</th>
-                        <th class="text-center">Pulse</th>
-                        <th class="text-center">Date</th>
-                        <th class="text-center">Severity</th>
-                        <th class="text-center">View Details</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($readings as $reading)
+        @if (count($readings) > 0)
+            <div class="table-responsive">
+                <table class="table table-striped table-hover table-condensed">
+                    <thead>
                         <tr>
-                            <td class="text-center">{{ $reading->systolic }}</td>
-                            <td class="text-center">{{ $reading->diastolic }}</td>
-                            <td class="text-center">{{ $reading->pulse }}</td>
-                            <td class="text-center">{{ date('M, j Y', strtotime($reading->reading_date)) }}</td>
-                            <td class="text-center">{{ $reading->severity() }}</td>
-                            <td class="text-center">
-                                <a href="{{ route('blood-pressure-details', ['id' => $reading->id]) }}">
-                                    <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
-                                </a>
-                            </td>
+                            <th class="text-center">SYS</th>
+                            <th class="text-center">DIA</th>
+                            <th class="text-center">Pulse</th>
+                            <th class="text-center">Date</th>
+                            <th class="text-center">Severity</th>
+                            <th class="text-center">View Details</th>
                         </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
+                    </thead>
+                    <tbody>
+                        @foreach($readings as $reading)
+                            <tr>
+                                <td class="text-center">{{ $reading->systolic }}</td>
+                                <td class="text-center">{{ $reading->diastolic }}</td>
+                                <td class="text-center">{{ $reading->pulse }}</td>
+                                <td class="text-center">{{ date('M, j Y', strtotime($reading->reading_date)) }}</td>
+                                <td class="text-center">{{ $reading->severity() }}</td>
+                                <td class="text-center">
+                                    <a href="{{ route('blood-pressure-details', ['id' => $reading->id]) }}">
+                                        <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+                                    </a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        @else
+            <div class="alert alert-warning">
+                There are no blood pressure readings.
+            </div>
+        @endif
     </div>
 </div>
