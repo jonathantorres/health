9f9389c4a8890504bcf14ebeac3a86616f57ceb1
Jonathan Torres <jonathantorres41@gmail.com>
2017-12-19 21:19:55 -0500

    Comment out registration of dusk service provider.

--- config/app.php
+++ config/app.php
@@ -168,7 +168,7 @@ return [
          */
         Laravel\Tinker\TinkerServiceProvider::class,
         Spatie\Backup\BackupServiceProvider::class,
-        Laravel\Dusk\DuskServiceProvider::class,
+        // Laravel\Dusk\DuskServiceProvider::class,
 
         /*
          * Application Service Providers...
