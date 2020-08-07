ab89759edad9922a4702f32821ae9e947085f8f8
Jonathan Torres <jonathantorres41@gmail.com>
2020-08-07 08:41:36 -0400

    Reorganize views

---
+++ views/blade/partials/footer.blade.php
@@ -0,0 +1,6 @@
+<hr>
+<div class="row">
+    <div class="col-md-12">
+        <p class="text-center">Copyrights &copy; {{ date('Y') }} | Jonathan Torres | {{ 'v' . $version }}</p>
+    </div>
+</div>
