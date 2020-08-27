c216b714a92474cb90c5850c129d3f7ced6733db
Jonathan Torres <jonathantorres41@gmail.com>
2020-08-27 19:43:51 -0400

    Register a new user

--- auth.go
+++ auth.go
@@ -92,11 +92,51 @@ func logout(res http.ResponseWriter, req *http.Request) {
 }
 
 func register(res http.ResponseWriter, req *http.Request) {
+	session := &Session{}
+	session.Start(res, req)
+	if loggedIn(session) {
+		http.Redirect(res, req, "/", http.StatusSeeOther)
+		return
+	}
+	db, err := initDb()
+	if err != nil {
+		serve500(res, req, err.Error())
+		return
+	}
+	if req.Method == "POST" {
+		req.ParseForm()
+		name := req.PostForm["name"][0]
+		lastName := req.PostForm["last_name"][0]
+		email := req.PostForm["email"][0]
+		pass := req.PostForm["password"][0]
+		passConfirm := req.PostForm["password_confirmation"][0]
+
+		if pass != passConfirm {
+			session.Set("errMsg", "Password and password confirmation must be the same")
+			http.Redirect(res, req, "/register", http.StatusSeeOther)
+			return
+		}
+		if err := registerUser(db, name, lastName, email, pass); err != nil {
+			session.Set("errMsg", err.Error())
+			http.Redirect(res, req, "/register", http.StatusSeeOther)
+			return
+		}
+		if err = authenticate(db, res, req, session, email, pass); err != nil {
+			session.Set("errMsg", err.Error())
+			http.Redirect(res, req, "/login", http.StatusSeeOther)
+			return
+		}
+		http.Redirect(res, req, "/", http.StatusSeeOther)
+		return
+	}
+
+	setErrorAndSuccessMessages(session)
 	res.Header().Set("Content-type", "text/html")
 	appData.LayoutData["PageTitle"] = "Health - Register"
 	if err := renderView("views/register.html", res); err != nil {
 		serveViewError(res, err)
 	}
+	cleanupErrorAndSuccessMessages(session)
 }
 
 func resetPassword(res http.ResponseWriter, req *http.Request) {
--- db.go
+++ db.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
+	"golang.org/x/crypto/bcrypt"
 	"time"
 )
 
@@ -24,4 +25,24 @@ func initDb() (*sql.DB, error) {
 		return nil, err
 	}
 	return db, nil
+}
+
+func registerUser(db *sql.DB, name, lastName, email, pass string) error {
+	sql := `
+		INSERT INTO users
+		(id, NAME, last_name, email, PASSWORD, remember_token, created_at, updated_at, deleted_at) 
+		VALUES(NULL, ?, ?, ?, ?, NULL, NOW(), NOW(), NULL);
+	`
+	hashedPass, err := bcrypt.GenerateFromPassword([]byte(pass), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	result, err := db.Exec(sql, name, lastName, email, hashedPass)
+	if err != nil {
+		return err
+	}
+	if _, err = result.RowsAffected(); err != nil {
+		return err
+	}
+	return nil
 }
