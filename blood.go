78b9fb3852c1cfe2bef33427a8e90dcf0f3a2f6d
Jonathan Torres <jonathantorres41@gmail.com>
2020-09-02 18:01:58 -0400

    Implement functionallity to create, delete and update blood readings

--- blood.go
+++ blood.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"net/http"
+	"strconv"
+	"time"
 )
 
 type BloodReading struct {
@@ -42,6 +44,10 @@ func (blood *BloodReading) Severity() *BloodSeverity {
 	}
 }
 
+func (blood *BloodReading) SqlDate() string {
+	return blood.Date[:10]
+}
+
 func bloodAdd(res http.ResponseWriter, req *http.Request) {
 	session := &Session{}
 	session.Start(res, req)
@@ -49,12 +55,39 @@ func bloodAdd(res http.ResponseWriter, req *http.Request) {
 		http.Redirect(res, req, "/login", http.StatusSeeOther)
 		return
 	}
+	db, err := initDb()
+	if err != nil {
+		serve500(res, req, err.Error())
+		return
+	}
+	user := getUserFromSession(session)
+
+	if req.Method == "POST" {
+		req.ParseForm()
+		nowDate := time.Now()
+		sys, _ := strconv.Atoi(req.PostForm["sys"][0])
+		dia, _ := strconv.Atoi(req.PostForm["dia"][0])
+		pulse, _ := strconv.Atoi(req.PostForm["pulse"][0])
+		date := req.PostForm["reading-date"][0]
+		date += " " + nowDate.Format("15:04:05")
+		if err := createBloodReading(db, user.Id, int32(sys), int32(dia), int32(pulse), date); err != nil {
+			session.Set("errMsg", err.Error())
+			http.Redirect(res, req, "/blood/add", http.StatusSeeOther)
+			return
+		}
+		session.Set("okMsg", "Blood reading has been created successfully!")
+		http.Redirect(res, req, "/", http.StatusSeeOther)
+		return
+	}
+
+	setErrorAndSuccessMessages(session)
 	res.Header().Set("Content-type", "text/html")
 	appData.LayoutData["PageTitle"] = "Health - Blood Pressure Add Reading"
 	appData.LayoutData["User"] = getUserFromSession(session)
 	if err := renderView("views/blood/add.html", res); err != nil {
 		serveViewError(res, err)
 	}
+	cleanupErrorAndSuccessMessages(session)
 }
 
 func bloodAll(res http.ResponseWriter, req *http.Request) {
@@ -126,17 +159,50 @@ func bloodEdit(res http.ResponseWriter, req *http.Request) {
 		http.Redirect(res, req, "/login", http.StatusSeeOther)
 		return
 	}
-	res.Header().Set("Content-type", "text/html")
-	appData.LayoutData["PageTitle"] = "Health - Edit Blood Pressure Reading"
-	appData.LayoutData["User"] = getUserFromSession(session)
-	_, err := getId(req.URL.Path) // todo: use id here
+	db, err := initDb()
+	if err != nil {
+		serve500(res, req, err.Error())
+		return
+	}
+	readingId, err := getId(req.URL.Path)
 	if err != nil {
 		serve404(res, req)
 		return
 	}
+	user := getUserFromSession(session)
+	reading, err := getBloodReading(db, user.Id, int64(readingId))
+	if err != nil {
+		serve500(res, req, err.Error())
+		return
+	}
+
+	if req.Method == "POST" {
+		req.ParseForm()
+		nowDate := time.Now()
+		sys, _ := strconv.Atoi(req.PostForm["sys"][0])
+		dia, _ := strconv.Atoi(req.PostForm["dia"][0])
+		pulse, _ := strconv.Atoi(req.PostForm["pulse"][0])
+		date := req.PostForm["reading-date"][0]
+		date += " " + nowDate.Format("15:04:05")
+		if err := updateBloodReading(db, user.Id, reading.Id, int32(sys), int32(dia), int32(pulse), date); err != nil {
+			session.Set("errMsg", err.Error())
+			http.Redirect(res, req, "/blood/edit/"+strconv.Itoa(readingId), http.StatusSeeOther)
+			return
+		}
+		session.Set("okMsg", "Blood reading has been updated successfully!")
+		http.Redirect(res, req, "/blood/edit/"+strconv.Itoa(readingId), http.StatusSeeOther)
+		return
+	}
+
+	setErrorAndSuccessMessages(session)
+	res.Header().Set("Content-type", "text/html")
+	appData.LayoutData["PageTitle"] = "Health - Edit Blood Pressure Reading"
+	appData.LayoutData["User"] = user
+	appData.ViewData["Reading"] = reading
 	if err := renderView("views/blood/edit.html", res); err != nil {
 		serveViewError(res, err)
 	}
+	cleanupErrorAndSuccessMessages(session)
 }
 
 func bloodDelete(res http.ResponseWriter, req *http.Request) {
@@ -146,11 +212,27 @@ func bloodDelete(res http.ResponseWriter, req *http.Request) {
 		http.Redirect(res, req, "/login", http.StatusSeeOther)
 		return
 	}
-	res.Header().Set("Content-type", "text/html")
-	_, err := getId(req.URL.Path) // todo: use id here
+	db, err := initDb()
 	if err != nil {
-		serve404(res, req) // todo: probably redirect instead of serving 404 page
+		serve500(res, req, err.Error())
 		return
 	}
-	res.Write([]byte("blood delete"))
+	readingId, err := getId(req.URL.Path)
+	if err != nil {
+		serve404(res, req)
+		return
+	}
+	user := getUserFromSession(session)
+	reading, err := getBloodReading(db, user.Id, int64(readingId))
+	if err != nil {
+		serve500(res, req, err.Error())
+		return
+	}
+	if err = deleteBloodReading(db, user.Id, reading.Id); err != nil {
+		session.Set("errMsg", err.Error())
+		http.Redirect(res, req, "/", http.StatusSeeOther)
+		return
+	}
+	session.Set("okMsg", "Blood pressure reading has been deleted!")
+	http.Redirect(res, req, "/", http.StatusSeeOther)
 }
